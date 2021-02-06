2e21b80f4da7e7b66fd5fd4503b1a8254b9abe72
caio <caio@clusterdesign.com>
2021-02-06 02:29:05 -0300

    conexao com mysql e criacao tabela atendimentos

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // funcao 
-const customExpress = require('./config/customExpress')
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const tabelas = require('./infraestrutura/tabelas');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = customExpress();
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro);
+    } else {
+        console.log("conectado com sucesso");
+        tabelas.init(conexao);
+        const app = customExpress();
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+    }
+})
+
+
