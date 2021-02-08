315043804bbd1ff3692212af211ea20756045c2f
caio <caio@clusterdesign.com>
2021-02-07 21:32:44 -0300

    tratamento de datas com moment js, retornando status http e validacao de dados antes de enviar para BD

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,44 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        console.log(dataCriacao)
+        const data = moment(atendimento.data, 'DD/MM/YYYY HH:mm').format('YYYY-MM-DD HH:mm:ss');
+        const atendimentoDatado = {...atendimento, dataCriacao, data}; 
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length > 3;
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 4 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        if(erros.length) {
+            res.status(400).json(erros)
+        } else {
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                   res.status(400).json(erro)
+                } else {
+                   res.status(201).json(resultados)
+                }
+            })
+        }
+
     }
 }
 
